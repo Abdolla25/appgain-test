3d90a9ef39c8c6d494a201e2a3e55369fe17e8ae
Abdolla25 <abdollaz225@gmail.com>
2023-02-02 14:26:31 +0200

    Added a Procfile.

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app: Application = express()
 app.use(express.json())
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://morning-forest-34474.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://morning-forest-34474.herokuapp.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
